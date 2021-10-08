1ed7c48c71476950f252fddaa0a629d55711d9af
Andre Luiz Torres <andre.torres.ti@outlook.com>
2021-10-07 22:55:51 -0300

    Ajuste Home

--- rpn_pi/src/pages/home/index.js
+++ rpn_pi/src/pages/home/index.js
@@ -1,11 +1,34 @@
 import React from "react";
-import {Card} from 'reactstrap'
+import {Card, Row, Col, Input, Button} from 'reactstrap'
 
 const Home = () => {
 
     return (
         <>
-<Card style={{background: "#000"}}>Home</Card>
+<Card style={{background: "#4e2779", height: "60px"}}>
+    <Row>
+        <Col sm="7"></Col>
+        <Col sm="5">
+            <div style={{marginTop: "12px", marginRight: "10px"}}>
+            <Row>
+            <Col sm="5"><Input
+            id="usuario"
+            name="usuario"
+            type="text"
+            >
+            </Input></Col>
+            <Col sm="5"><Input
+            id="usuario"
+            name="usuario"
+            type="text"
+            >
+            </Input></Col>
+            <Col sm="2"><Button>Login</Button></Col>
+            </Row>
+            </div>
+            </Col>
+    </Row>
+</Card>
         </>        
     )
 
